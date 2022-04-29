d2dc5e1b8ad53c53ccb2c763a983d67940e31522
evilqueen599 <triciaaloufi@gmail.com>
2022-04-29 18:13:44 -0400

    Basic

---
+++ src/Main/Main.java
@@ -0,0 +1,38 @@
+package Main;
+/**
+ * Main method
+ */
+
+/**
+ * @author Tricia Aloufi
+ */
+import helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/** initializes the main method and accesses the login screen GUI.
+ *
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/loginScreen.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setTitle("User Login");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        JDBC.openConnection();
+        JDBC.closeConnection();
+    }
+}
+
---
+++ src/controllers/loginController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void handleExit (ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
