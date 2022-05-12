a0a2eccb12c52a727173a9e3e4bcb6d30347d46a
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-12 19:41:07 -0400

    Merge remote-tracking branch 'origin/master'

--- src/controllers/ModifyCustomerController.java
+++ src/controllers/ModifyCustomerController.java
@@ -1,12 +1,27 @@
 package controllers;
 
+import Models.Countries;
+import Models.FirstLevelDivisions;
+import database.CountriesDAO;
+import database.FirstLevelDivisionDAO;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 
-public class ModifyCustomerController {
+public class ModifyCustomerController implements Initializable {
 
     @FXML
     private TextField modCustomerIdTxt;
@@ -24,14 +39,15 @@ public class ModifyCustomerController {
     private TextField modPhoneTxt;
 
     @FXML
-    private ComboBox<String> countryCombo;
+    private ComboBox<Countries> countryCombo;
 
     @FXML
-    private ComboBox<String> stateCombo;
+    private ComboBox<FirstLevelDivisions> stateCombo;
 
 
 
     public void handleSave(ActionEvent actionEvent) {
+
     }
 
     public void handleClear(ActionEvent actionEvent) {
@@ -44,9 +60,40 @@ public class ModifyCustomerController {
         stateCombo.getSelectionModel().clearSelection();
     }
 
-    public void handleCancel(ActionEvent actionEvent) {
+    public void handleCancel(ActionEvent actionEvent) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Cancel");
+        alert.setHeaderText("Are sure you wish to cancel modifying this customer profile?");
+        alert.setContentText("If yes, press OK to return to the main screen.");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            Stage stage = ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow());
+            Parent scene = FXMLLoader.load(getClass().getResource("/customerMenu.FXML"));
+            stage.setTitle("Customer Menu");
+            stage.setScene(new Scene(scene));
+            stage.show();
+            stage.centerOnScreen();
+        }
     }
 
-    public void handleFilterStates(ActionEvent actionEvent) {
+    public void handleFilterStates(ActionEvent actionEvent) throws SQLException {
+        try {
+            ObservableList<FirstLevelDivisions> divisions = FirstLevelDivisionDAO.getFirstLevel();
+            stateCombo.setItems(divisions);
+
+            ObservableList<FirstLevelDivisions> division = FirstLevelDivisionDAO.returnDivisionCountry
+                    (countryCombo.getSelectionModel().getSelectedItem().getCountryId());
+            stateCombo.setItems(division);
+        } catch (NullPointerException nullPointerException) {
+            stateCombo.setItems(null);
+        }
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Countries> countries = CountriesDAO.getCountryId();
+        countryCombo.setItems(countries);
+
+        ObservableList<FirstLevelDivisions> firstLevelDivisions = FirstLevelDivisionDAO.getFirstLevel();
+        stateCombo.setItems(firstLevelDivisions);
     }
 }
