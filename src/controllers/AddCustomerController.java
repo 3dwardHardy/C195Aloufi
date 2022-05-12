aaf500904f96d708c95ef1c25945b92b9d3d98d3
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-12 00:06:10 -0400

    revised

--- src/controllers/AddCustomerController.java
+++ src/controllers/AddCustomerController.java
@@ -1,4 +1,59 @@
 package controllers;
 
-public class AddCustomerController {
+import Models.Countries;
+import Models.FirstLevelDivisions;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCustomerController implements Initializable {
+    @FXML
+    private TextField customerIdTxt;
+
+    @FXML
+    private TextField customerNameTxt;
+
+    @FXML
+    private TextField customerAddressTxt;
+
+    @FXML
+    private TextField postalTxt;
+
+    @FXML
+    private TextField customerPhoneTxt;
+
+    @FXML
+    private ComboBox<Countries> countryCombo;
+
+    @FXML
+    private ComboBox<FirstLevelDivisions> stateCombo;
+
+    public void handleFirstLevelDivision(ActionEvent actionEvent) {
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+        customerIdTxt.setText("");
+        customerNameTxt.setText("");
+        customerAddressTxt.setText("");
+        postalTxt.setText("");
+        countryCombo.getSelectionModel().clearSelection();
+        stateCombo.getSelectionModel().clearSelection();
+        customerPhoneTxt.setText("");
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
