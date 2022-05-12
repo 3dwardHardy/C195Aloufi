41fd2e3b3a5a2ea4da6656ba8f34ba8bc0b59e2d
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-12 19:36:04 -0400

    Merge remote-tracking branch 'origin/master'

--- src/controllers/ModifyCustomerController.java
+++ src/controllers/ModifyCustomerController.java
@@ -1,4 +1,52 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+
 public class ModifyCustomerController {
+
+    @FXML
+    private TextField modCustomerIdTxt;
+
+    @FXML
+    private TextField modCustomerNameTxt;
+
+    @FXML
+    private TextField modCustomerAddressTxt;
+
+    @FXML
+    private TextField modPostalTxt;
+
+    @FXML
+    private TextField modPhoneTxt;
+
+    @FXML
+    private ComboBox<String> countryCombo;
+
+    @FXML
+    private ComboBox<String> stateCombo;
+
+
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+        modCustomerIdTxt.setText("");
+        modCustomerNameTxt.setText("");
+        modCustomerAddressTxt.setText("");
+        modPostalTxt.setText("");
+        modPhoneTxt.setText("");
+        countryCombo.getSelectionModel().clearSelection();
+        stateCombo.getSelectionModel().clearSelection();
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleFilterStates(ActionEvent actionEvent) {
+    }
 }
