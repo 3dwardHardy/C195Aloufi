a050479ea9faa9979664c7fb609d53d52c1c4697
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-13 11:13:36 -0400

    Merge remote-tracking branch 'origin/master'

--- src/controllers/AddAppointmentController.java
+++ src/controllers/AddAppointmentController.java
@@ -1,4 +1,14 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+
 public class AddAppointmentController {
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+    }
 }
--- src/controllers/ModifyAppointmentController.java
+++ src/controllers/ModifyAppointmentController.java
@@ -1,4 +1,14 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+
 public class ModifyAppointmentController {
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleClear(ActionEvent actionEvent) {
+    }
 }
