a2628d831a6ae8cc628cdaa5052d518dd84ba52b
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-16 06:58:19 -0400

    all combos functional all exceptions complete

--- src/helper/Logger.java
+++ src/helper/Logger.java
@@ -1,7 +1,38 @@
 package helper;
 
+import database.UsersDAO;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Logger {
+    public static void loginAttempts(String userName, String password) {
+        BufferedWriter writer = null;
 
-
-
+        try {
+            writer = new BufferedWriter(new FileWriter("login_activity.txt"));
+            boolean trackLogins = UsersDAO.validLogin(userName, password);
+            String loginSuccess;
+            if (trackLogins) {
+                loginSuccess = "Username: " + userName + " had a successful login at: " + LocalDateTime.now() + "\n";
+                writer.write(loginSuccess);
+            } else {
+                loginSuccess = "Username: " + userName + " attempted to login at: " + LocalDateTime.now() +
+                        " but was unsuccessful.\n";
+                writer.write(loginSuccess);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
 }
