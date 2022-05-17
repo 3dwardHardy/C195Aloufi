c72d0373466ce8a278fd288a155756e8a8db5e58
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-17 13:44:19 -0400

    fixed lambda logic for both!!!! project complete sans notes!!!

--- src/controllers/ReportsController.java
+++ src/controllers/ReportsController.java
@@ -10,7 +10,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ReportsController {
-
+    /**
+     * On user click opens the screen to select and display a report containing all appointments by a user selected type.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void handleByTypeMonth(ActionEvent actionEvent) throws IOException{
         Stage stage = ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow());
         Parent scene = FXMLLoader.load(getClass().getResource("/typeMonthReport.FXML"));
@@ -20,6 +24,11 @@ public class ReportsController {
         stage.centerOnScreen();
     }
 
+    /**
+     * On click opens the screen to allow the user to generate reports by the user selected contact.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void handleByContact(ActionEvent actionEvent) throws IOException {
         Stage stage = ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow());
         Parent scene = FXMLLoader.load(getClass().getResource("/apptsByContact.FXML"));
@@ -29,6 +38,11 @@ public class ReportsController {
         stage.centerOnScreen();
     }
 
+    /**
+     * On click opens a menu to allow the user to generate reports by the user selected customer.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void handleByCustomer(ActionEvent actionEvent) throws IOException{
         Stage stage = ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow());
         Parent scene = FXMLLoader.load(getClass().getResource("/appointmentsByCustomer.FXML"));
@@ -38,6 +52,11 @@ public class ReportsController {
         stage.centerOnScreen();
     }
 
+    /**
+     * Handles a back button click event and sends the user back to the programs main screen.
+     * @param actionEvent
+     * @throws IOException
+     */
     public void handleBack(ActionEvent actionEvent) throws IOException {
         Stage stage = ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow());
         Parent scene = FXMLLoader.load(getClass().getResource("/mainScreen.FXML"));
