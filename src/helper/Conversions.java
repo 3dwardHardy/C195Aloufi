862520ad67c1637d17f1c36b3e3abfd4d7265faa
evilqueen599 <triciaalhammadi@gmail.com>
2022-05-17 14:45:37 -0400

    fixed lambda logic for both!!!! project complete sans notes!!!

--- src/helper/Conversions.java
+++ src/helper/Conversions.java
@@ -6,7 +6,9 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 public class Conversions {
-
+    /**
+     * Handles calls for the out of office hours error screen.
+     */
     public static void outOfOfficeHours() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Appointment Error");
@@ -15,6 +17,10 @@ public class Conversions {
         alert.showAndWait();
     }
 
+    /**
+     * Callable method sets the format for the current time stamps
+     * @return
+     */
     public static Timestamp getCurrentTimestamp() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
